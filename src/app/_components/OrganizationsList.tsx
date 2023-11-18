2263fa3712f81050e1748bae47af61d612b59860
Trevor Keith <tk@tkeith.com>
2023-11-18 20:58:12 +0300

    Codapt.ai: Fixed organization list erroneously adding dummy data organizations.
    Codapt-Request: KCW1K3SnDdG5YygYrZhfUt

--- src/app/_components/OrganizationsList.tsx
+++ src/app/_components/OrganizationsList.tsx
@@ -6,30 +6,12 @@ import OrganizationCard from "~/app/_components/OrganizationCard";
 
 export default function OrganizationsList() {
   const organizationsQuery = api.getOrganizations.useQuery(null);
-  const addOrganizationMutation = api.addOrganization.useMutation();
 
   useEffect(() => {
     organizationsQuery.refetch().catch((error) => {
       console.error("Error refetching organizations:", error);
     });
   }, [organizationsQuery]);
-
-  useEffect(() => {
-    addOrganizationMutation
-      .mutateAsync({
-        name: "Organization Name",
-        walletAddress: "0x123...",
-        chainId: "1",
-      })
-      .then(() => {
-        organizationsQuery.refetch().catch((error) => {
-          console.error("Error refetching organizations after adding:", error);
-        });
-      })
-      .catch((error) => {
-        console.error("Error adding organization:", error);
-      });
-  }, [addOrganizationMutation, organizationsQuery]);
 
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
