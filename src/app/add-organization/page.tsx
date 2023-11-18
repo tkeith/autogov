0f94a4befe0aedbcbdf45c599e20c65bc5271ed0
Trevor Keith <tk@tkeith.com>
2023-11-18 21:16:55 +0300

    Codapt.ai: Added styling to the add organization form
    Codapt-Request: eqxmrtAkQNh1iaJ6nLQsTS

--- src/app/add-organization/page.tsx
+++ src/app/add-organization/page.tsx
@@ -16,24 +16,39 @@ export default function AddOrganizationPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
+    <form
+      onSubmit={handleSubmit}
+      className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md"
+    >
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </label>
+      </div>
+      <div className="mb-6">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Chain ID:
+          <input
+            type="text"
+            value={chainId}
+            onChange={(e) => setChainId(e.target.value)}
+            className="w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </label>
+      </div>
+      <div className="flex items-center justify-between">
         <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          type="submit"
+          value="Submit"
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-      </label>
-      <label>
-        Chain ID:
-        <input
-          type="text"
-          value={chainId}
-          onChange={(e) => setChainId(e.target.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
+      </div>
     </form>
   );
 }
