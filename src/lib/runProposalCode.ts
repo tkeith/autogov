b372695cb0dd6faacb99a0725cbf4f67981bd76b
Trevor Keith <tk@tkeith.com>
2023-11-19 02:43:18 +0300

    run code helper function

---
+++ src/lib/runProposalCode.ts
@@ -0,0 +1,56 @@
+import { db } from "~/server/db";
+import fs from "fs";
+import { spawn } from "child_process";
+
+const runProposalCode = async (id: number) => {
+  // grab the proposal
+  const proposal = await db.proposal.findUniqueOrThrow({
+    where: { id },
+    select: { code: true },
+  });
+
+  // update status to 'implementing'
+  await db.proposal.update({
+    where: { id },
+    data: {
+      status: "implementing",
+    },
+  });
+
+  const code = proposal.code;
+
+  // save the code in tmp-proposal-code.js
+  fs.writeFileSync("./tmp-proposal-code.js", code);
+  let output = "";
+  try {
+    // run the code by running `node tmp-proposal-code.js` using child_process
+    // wait for the child process to finish, and get stdout & stderr and combine them
+    // into a single string
+    const child = spawn("node", ["./tmp-proposal-code.js"]);
+
+    child.stdout.on("data", (data) => {
+      output += data;
+    });
+    child.stderr.on("data", (data) => {
+      output += data;
+    });
+    // wait for complete
+    await new Promise((resolve) => {
+      child.on("close", resolve);
+    });
+  } finally {
+    // delete the tmp-proposal-code.js file
+    fs.unlinkSync("./tmp-proposal-code.js");
+  }
+
+  // update the database
+  await db.proposal.update({
+    where: { id },
+    data: {
+      codeResult: output,
+      status: "implimented",
+    },
+  });
+};
+
+export default runProposalCode;
