edd783600c05d3f4c72fab7dd3d9d9765c539631
Trevor Keith <tk@tkeith.com>
2023-11-18 20:15:16 +0300

    Codapt.ai: Added `chains` constant with Ethereum and Polygon EVM blockchains details.
    Codapt-Request: N6ycJDhS8qbr2CtKLSLGAp

---
+++ src/lib/chains.ts
@@ -0,0 +1,14 @@
+const chains = [
+  {
+    name: "Ethereum",
+    chainId: 1,
+    rpc: "https://mainnet.infura.io/v3/YOUR-PROJECT-ID",
+  },
+  {
+    name: "Polygon",
+    chainId: 137,
+    rpc: "https://rpc-mainnet.maticvigil.com/",
+  },
+];
+
+export default chains;
