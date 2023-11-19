4ab13d6968ed82027b17fa2c642c561562519d3b
Trevor Keith <tk@tkeith.com>
2023-11-19 04:10:59 +0300

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cookies } from "next/headers";
 import { TRPCReactProvider } from "~/trpc/react";
 import Web3Container from "~/app/_components/Web3Container";
 import { Toaster } from "react-hot-toast";
+import Link from "next/link";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,7 +29,14 @@ export default function RootLayout({
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider cookies={cookies().toString()}>
           <div className="mx-auto my-8 flex max-w-3xl flex-col space-y-4 p-4 ">
-            <Web3Container>{children}</Web3Container>
+            <Web3Container>
+              <Link href="/">
+                <h1 className="cursor-pointer text-3xl font-bold">
+                  AutoGovern
+                </h1>
+              </Link>
+              {children}
+            </Web3Container>
           </div>
         </TRPCReactProvider>
         <Toaster />
